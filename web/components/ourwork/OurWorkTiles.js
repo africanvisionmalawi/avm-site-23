cd18b93ca0745076d6cb1b42179c814f3a07cda8
Nigel Bunner <nigelbunner@gmail.com>
2023-03-05 08:52:49 +0000

    Adds images to our work tiles

--- web/components/ourwork/OurWorkTiles.js
+++ web/components/ourwork/OurWorkTiles.js
@@ -1,6 +1,6 @@
 // import BackgroundImage from "gatsby-background-image";
-import styled from "@emotion/styled";
 // import { graphql, Link, useStaticQuery } from "gatsby";
+import styled from "@emotion/styled";
 
 export const OurWorkTiles = ({ currentProject, displayHeading }) => {
   // const featuredImage = useStaticQuery(
@@ -54,12 +54,61 @@ export const OurWorkTiles = ({ currentProject, displayHeading }) => {
               className={project.fullWidth ? "fullWidth" : "halfSize"}
               key={project.id}
             >
-              <a href={project.src}>
+              <a href={project.src} className={project.id}>
                 <Overlay>
                   <SubHeading>
                     {project.name} {project.fullWidth}
                   </SubHeading>
                 </Overlay>
+                {/* <picture>
+                  <source
+                    type="image/webp"
+                    media="(min-width: 576px)"
+                    srcset="/static/d085284b2474ab03be045a7b311a1c3d/626c1/mainphoto_village-ver3.webp 245w,
+/static/d085284b2474ab03be045a7b311a1c3d/9dacf/mainphoto_village-ver3.webp 490w,
+/static/d085284b2474ab03be045a7b311a1c3d/a54f6/mainphoto_village-ver3.webp 980w,
+/static/d085284b2474ab03be045a7b311a1c3d/b0da7/mainphoto_village-ver3.webp 1140w"
+                    sizes="(max-width: 980px) 100vw, 980px"
+                  />
+                  <source
+                    media="(min-width: 576px)"
+                    srcset="/static/d085284b2474ab03be045a7b311a1c3d/e2305/mainphoto_village-ver3.jpg 245w,
+/static/d085284b2474ab03be045a7b311a1c3d/45f69/mainphoto_village-ver3.jpg 490w,
+/static/d085284b2474ab03be045a7b311a1c3d/fe322/mainphoto_village-ver3.jpg 980w,
+/static/d085284b2474ab03be045a7b311a1c3d/d5ea2/mainphoto_village-ver3.jpg 1140w"
+                    sizes="(max-width: 980px) 100vw, 980px"
+                  />
+                  <source
+                    type="image/webp"
+                    srcset="/static/d085284b2474ab03be045a7b311a1c3d/ec243/mainphoto_village-ver3.webp 120w,
+/static/d085284b2474ab03be045a7b311a1c3d/1c752/mainphoto_village-ver3.webp 240w,
+/static/d085284b2474ab03be045a7b311a1c3d/3799f/mainphoto_village-ver3.webp 480w,
+/static/d085284b2474ab03be045a7b311a1c3d/223e8/mainphoto_village-ver3.webp 720w,
+/static/d085284b2474ab03be045a7b311a1c3d/5c4e3/mainphoto_village-ver3.webp 960w,
+/static/d085284b2474ab03be045a7b311a1c3d/86038/mainphoto_village-ver3.webp 1140w"
+                    sizes="(max-width: 480px) 100vw, 480px"
+                  />
+                  <source
+                    srcset="/static/d085284b2474ab03be045a7b311a1c3d/3c294/mainphoto_village-ver3.jpg 120w,
+/static/d085284b2474ab03be045a7b311a1c3d/d8d74/mainphoto_village-ver3.jpg 240w,
+/static/d085284b2474ab03be045a7b311a1c3d/a58c7/mainphoto_village-ver3.jpg 480w,
+/static/d085284b2474ab03be045a7b311a1c3d/fe031/mainphoto_village-ver3.jpg 720w,
+/static/d085284b2474ab03be045a7b311a1c3d/3c58a/mainphoto_village-ver3.jpg 960w,
+/static/d085284b2474ab03be045a7b311a1c3d/6a999/mainphoto_village-ver3.jpg 1140w"
+                    sizes="(max-width: 480px) 100vw, 480px"
+                  />
+                  <img
+                    sizes="(max-width: 980px) 100vw, 980px"
+                    srcset="/static/d085284b2474ab03be045a7b311a1c3d/e2305/mainphoto_village-ver3.jpg 245w,
+/static/d085284b2474ab03be045a7b311a1c3d/45f69/mainphoto_village-ver3.jpg 490w,
+/static/d085284b2474ab03be045a7b311a1c3d/fe322/mainphoto_village-ver3.jpg 980w,
+/static/d085284b2474ab03be045a7b311a1c3d/d5ea2/mainphoto_village-ver3.jpg 1140w"
+                    src="/static/d085284b2474ab03be045a7b311a1c3d/fe322/mainphoto_village-ver3.jpg"
+                    alt=""
+                    loading="lazy"
+                    style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: contain; object-position: center center; opacity: 1; transition: opacity 500ms ease 0s;"
+                  />
+                </picture> */}
                 {/* <Img
                   fluid={
                     project.hasMobileImage === true
@@ -98,6 +147,7 @@ const projects = [
     src: "/sams-village/",
     imageIdDesktop: "PhotoVillageDesktop",
     imageIdMobile: "PhotoVillageMobile",
+    imgSrcDesktop: "mainphoto_village-ver3",
     colWidth: {
       base: "100%",
       md: "100%",
@@ -181,6 +231,7 @@ const TileCont = styled.div`
 `;
 
 const Tile = styled.div`
+  height: 228px;
   overflow: hidden;
   padding: 4px;
   position: relative;
@@ -199,6 +250,7 @@ const Tile = styled.div`
   & a {
     border-radius: 4px;
     display: block;
+    height: 100%;
     overflow: hidden;
     position: relative;
     img {
@@ -206,6 +258,30 @@ const Tile = styled.div`
       height: 100%;
       width: 100%;
     }
+  }
+  & a.health {
+    background: url(/img/mainphoto_health.jpg) 0 0 no-repeat;
+    background-size: cover;
+  }
+  & a.village {
+    background: url(/img/mainphoto_village-ver3.jpg) 0 0 no-repeat;
+    background-size: cover;
+  }
+  & a.water {
+    background: url(/img/mainphoto_water.jpg) 0 0 no-repeat;
+    background-size: cover;
+  }
+  & a.education {
+    background: url(/img/mainphoto_education.jpg) 0 0 no-repeat;
+    background-size: cover;
+  }
+  & a.environment {
+    background: url(/img/mainphoto_environment.jpg) 0 0 no-repeat;
+    background-size: cover;
+  }
+  & a.celebrate {
+    background: url(/img/mainphoto_celebrate.jpg) 0 0 no-repeat;
+    background-size: cover;
   }
 `;
 
