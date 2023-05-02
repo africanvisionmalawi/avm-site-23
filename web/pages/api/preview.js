be02b12aafbda93b9e65ecbdbd1d4663a0571e59
Nigel Bunner <nigelbunner@gmail.com>
2023-05-02 08:51:23 +0100

    Update preview.js

--- web/pages/api/preview.js
+++ web/pages/api/preview.js
@@ -4,7 +4,7 @@ export default async function preview(req, res) {
   // The secret can't be stored in an env variable with a NEXT_PUBLIC_ prefix, as it would make you vulnerable to leaking the token to anyone.
   // If you don't have an custom API with authentication that can handle checking secrets, you may use https://github.com/sanity-io/sanity-studio-secrets to store the secret in your dataset.
   // const secret = await getSecret();
-  const secret = "";
+  const secret = process.env.NEXT_PUBLIC_SANITY_STUDIO_READ_TOKEN;
 
   // This is the most common way to check for auth, but we encourage you to use your existing auth infra to protect your token and securely transmit it to the client
   if (!req.query.secret || req.query.secret !== secret) {
